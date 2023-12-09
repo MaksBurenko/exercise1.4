1fa0139936839ee6a30a4282c46b7899ac8ae077
MaksBurenko <Cherniymaks@yandex.ru>
2023-12-09 15:59:21 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,18 @@ public class Main {
         int j = 0;
         int n = 0;
         int k = 0;
+        long sumOld = 0;
+        long sumNew = 0;
+        long result = 0;
+
         List <Long> list = new ArrayList<>();
+
+        Map<Integer, ArrayList<Long>> map = new TreeMap<>();
 
         try {
 
      //   Scanner scanner = new Scanner(System.in);
-            Scanner scanner = new Scanner("5 2" + "\n" + "1  2  1  3  5");
+            Scanner scanner = new Scanner("5 2" + "\n" + "1  7  10  5  777 555 525 12");
 
         while (scanner.hasNextLine() && i<2) {
             switch (i) {
@@ -32,20 +38,31 @@ public class Main {
 
         scanner.close();
 
+        for (long temp : list) {
+            sumOld += temp;
+            map.put(scanNumbers(temp), new ArrayList<>());
+        }
+
+        for(long temp : list){
+            int t = scanNumbers(temp);
+            for (Integer key : map.keySet()) {
+               if(t==key){
+                   map.get(key).add(temp);
+               }
+            }
+        }
+
+            System.out.println(map);
+
         } catch (IndexOutOfBoundsException | NoSuchElementException e) {
             e.printStackTrace(System.out);
         }
     }
+
+    public static int scanNumbers (long number){
+        return (int)Math.log10(number) + 1;
+    }
 }
-
-
-
-
-
-
-
-    int number = 123456;
-    int numDigits = (int)Math.log10(number) + 1;
 
 
 
